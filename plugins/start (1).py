6fb10ac26d3eec295a319103508c4965db815dd1
GTCHANNELS <108328260+GTCHANNELS@users.noreply.github.com>
2024-06-13 15:13:07 +0530

    Update start (1).py

--- plugins/start (1).py
+++ plugins/start (1).py
@@ -125,9 +125,8 @@ REPLY_ERROR = """<code>Use this command as a replay to any telegram message with
 async def not_joined(client: Client, message: Message):
     buttons = [
         [
-            InlineKeyboardButton(
-                "Jᴏɪɴ Cʜᴀɴɴᴇʟ",
-                url = client.invitelink)
+            InlineKeyboardButton(text="Jᴏɪɴ Cʜᴀɴɴᴇʟ 𝟷", url=client.invitelink),
+            InlineKeyboardButton(text="Jᴏɪɴ Cʜᴀɴɴᴇʟ 𝟸", url=client.invitelink2),
         ]
     ]
     try:

569525323383a98f81496c3067212ffe8a6df35c
GTCHANNELS <108328260+GTCHANNELS@users.noreply.github.com>
2024-06-13 14:17:19 +0530

    Update config.py

--- config.py
+++ config.py
@@ -33,6 +33,7 @@ DB_NAME = os.environ.get("DATABASE_NAME", "Mongodb")
 
 #force sub channel id, if you want enable force sub
 FORCE_SUB_CHANNEL = int(os.environ.get("FORCE_SUB_CHANNEL", "-1002106420058"))
+FORCESUB_CHANNEL2 = int(os.environ.get("FORCESUB_CHANNEL2", "0"))
 
 TG_BOT_WORKERS = int(os.environ.get("TG_BOT_WORKERS", "4"))
 
